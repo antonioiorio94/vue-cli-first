0e58c2555b4693d68d28e57d11cf19ce0ce73eff
Antonio Iorio <antonioiorio94@gmail.com>
2022-04-05 18:11:21 +0200

    fix prettier + aggiunta title e modifica css

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Title />
+  </div>
+</template>
+
+<script>
+import Title from "./components/Title.vue";
+
+export default {
+  name: "App",
+  components: {
+    Title,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>Ciao!</h1>
+    <p>La mia prima app Vue CLI!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  font-size: 5rem;
+  background-color: rgb(65 184 131);
+}
+
+p {
+  margin-top: 20px;
+}
+</style>
